b8a6b3ac4cda08fdab5d4f4999ca6da74023357b
TejaswiniNuggu <tejaswininuggu357@gmail.com>
2022-07-16 17:34:11 +0530

    spelling error

--- src/AddMovie.js
+++ src/AddMovie.js
@@ -1,40 +1,61 @@
 // import { useState } from "react";
 import { useEffect, useState } from "react";
-import TextField from '@mui/material/TextField';
+import TextField from "@mui/material/TextField";
 import { useForm } from "react-hook-form";
-import {API} from "./global";
-
+import { API } from "./global";
 
 // export function AddMovie({ movieList, setMoiveList }) {
-  export function AddMovie() {
+export function AddMovie() {
   const [name, setName] = useState("");
   const [poster, setPoster] = useState("");
   const [summary, setSummary] = useState("");
   const [rating, setRating] = useState("");
   const [trailer, setTrailer] = useState("");
-  const addMovie =  ()=>{
-    const newMovie = { name,  poster,summary,rating,  trailer };
+  const addMovie = () => {
+    const newMovie = { name, poster, summary, rating, trailer };
     console.log(newMovie);
-    fetch(`${API}/movies`,{
-  method:"POST",
-  body: JSON.stringify(newMovie),
-  headers:{
-    "Content-Type":"aplplication/json",
-  },
-  }).then((data)=>data.json());
+
+    fetch(`${API}/movies`, {
+      method: "POST",
+      body: JSON.stringify([newMovie]),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((data) => data.json());
   };
   return (
     <div>
       <div className="add-movie-form">
-        <TextField onChange={(event) => setName(event.target.value)} variant="filled" label="Name"  />
-        <input onChange={(event) => setPoster(event.target.value)} required={true}  type="text" placeholder="Poster" />
-        <input onChange={(event) => setSummary(event.target.value)} type="text" placeholder="Summary" />
-        <input onChange={(event) => setRating(event.target.value)} type="text" placeholder="Rating" />
-        <input onChange={(event) => setTrailer(event.target.value)} type="text" placeholder="Trailer" />
-        <button onClick= {addMovie}
-        variant="contained">Add Movie</button>
+        <TextField
+          onChange={(event) => setName(event.target.value)}
+          variant="filled"
+          label="Name"
+        />
+        <input
+          onChange={(event) => setPoster(event.target.value)}
+          required={true}
+          type="text"
+          placeholder="Poster"
+        />
+        <input
+          onChange={(event) => setSummary(event.target.value)}
+          type="text"
+          placeholder="Summary"
+        />
+        <input
+          onChange={(event) => setRating(event.target.value)}
+          type="text"
+          placeholder="Rating"
+        />
+        <input
+          onChange={(event) => setTrailer(event.target.value)}
+          type="text"
+          placeholder="Trailer"
+        />
+        <button onClick={addMovie} variant="contained">
+          Add Movie
+        </button>
       </div>
-
     </div>
   );
 }
